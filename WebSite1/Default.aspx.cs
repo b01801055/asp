b61cf14ca40fa691fb517802a2142c38055a647c
b01801055 <45553512+b01801055@users.noreply.github.com>
2022-09-12 23:57:12 +0800

    Initial commit

---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int a = Convert.ToInt32(Request["a"]);
+
+        if(a > 10)
+        {
+            Response.Write("a > 10");
+        }
+        else
+        {
+            Response.Write("a < 10");
+        }
+    }
+}
