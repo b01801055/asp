d79221690d425f64fdd6d7bec3bed9847fec657f
b01801055 <45553512+b01801055@users.noreply.github.com>
2022-10-06 23:05:24 +0800

    04-3
    start

---
+++ WebSite1/Default40.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default40.aspx.cs" Inherits="Default40" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            數字1:<asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            數字2:<asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button_Click" Text="+" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="-" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button3" runat="server" Text="*" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button4" runat="server" Text="/" />
+            <br />
+            <br />
+            <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="=" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default40.aspx.cs
@@ -0,0 +1,79 @@
+﻿using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default40 : System.Web.UI.Page
+{
+    int number1;
+    int number2;
+    double result;
+    string math = "";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        switch (math)
+        {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = Convert.ToDouble(number1) / Convert.ToDouble(number2);
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        Label1.Text = result.ToString();
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        number1 = Convert.ToInt32(TextBox1.Text);
+    }
+
+    protected void TextBox2_TextChanged(object sender, EventArgs e)
+    {
+        number2 = Convert.ToInt32(TextBox2.Text);
+    }
+
+    protected void Button_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+
+        switch (btn.Text.ToString())
+        {
+            case "+":
+                math += "+";
+                Response.Write(number1+math+number2);
+                break;
+            case "-":
+                math += "-";
+                break;
+            case "*":
+                math += "*";
+                break;
+            case "/":
+                math += "/";
+                break;
+            default:
+                math += "@";
+                break;
+        }
+    }
+}
