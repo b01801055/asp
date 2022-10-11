4a42db6fe8c1475f25fb5247e54ae5f5ee0f3528
b01801055 <45553512+b01801055@users.noreply.github.com>
2022-10-11 21:30:28 +0800

    05-2
    1:07:53

---
+++ WebSite1/Default42.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default42 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Class1.defense();
+
+        Class2 x = new Class2();
+        x.defense2();
+    }
+}
